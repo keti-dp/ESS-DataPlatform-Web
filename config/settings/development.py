61bfbb9623d1f6de8cedd154cdd4df7f89b4a01e
thinklim <sagalim3983@gmail.com>
2021-10-18 11:27:59 +0900

    Split development and production environment setting

---
+++ config/settings/development.py
@@ -0,0 +1,6 @@
+# SECURITY WARNING: don't run with debug turned on in production!
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
--- manage.py
+++ manage.py
@@ -3,10 +3,13 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -19,4 +22,5 @@ def main():
 
 
 if __name__ == '__main__':
+    load_dotenv()
     main()
