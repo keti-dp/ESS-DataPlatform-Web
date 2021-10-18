6c3a38aa5b11c92445eb509ee4b9ee5cd85be6a3
thinklim <sagalim3983@gmail.com>
2021-10-18 15:44:29 +0900

    Add default web template

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import DashboardView, IndexView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", IndexView.as_view(), name="index"),
+    path("dashboard/", DashboardView.as_view(), name="dashboard"),
 ]
