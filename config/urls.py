7cdda41ce749d726f9d5f02bc448bc8e9c78c039
thinklim <sagalim3983@gmail.com>
2021-10-26 17:33:37 +0900

    Add read-API by period of ESS operation data

--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from .views import DashboardView, IndexView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", IndexView.as_view(), name="index"),
     path("dashboard/", DashboardView.as_view(), name="dashboard"),
+    path("api/ess/", include("ess.urls"), name="ess"),
 ]
--- ess/views.py
+++ ess/views.py
@@ -1,3 +1,78 @@
-from django.shortcuts import render
+from datetime import datetime
+from datetime import timedelta
+from rest_framework.generics import ListAPIView
+from .models import Bank, Etc, Pcs, Rack
+from .paginations import LargeResultsSetPagination
+from .serializer import BankSerializer, EtcSerializer, PcsSerializer, RackSerializer
 
-# Create your views here.
+
+class BankListView(ListAPIView):
+    serializer_class = BankSerializer
+    pagination_class = LargeResultsSetPagination
+
+    def get_queryset(self):
+        operation_num = self.kwargs["operation_num"]
+        bank_id = self.kwargs["bank_id"]
+        date = self.request.query_params.get("date")
+
+        queryset = Bank.objects.filter(bank_id=bank_id).order_by("timestamp")
+
+        if date is not None:
+            new_date = datetime.strptime(date, "%Y-%m-%d") + timedelta(days=1)
+            queryset = queryset.filter(bank_id=bank_id, timestamp__gte=date, timestamp__lt=new_date)
+
+        return queryset
+
+
+class EtcListView(ListAPIView):
+    serializer_class = EtcSerializer
+    pagination_class = LargeResultsSetPagination
+
+    def get_queryset(self):
+        operation_num = self.kwargs["operation_num"]
+        bank_id = self.kwargs["bank_id"]
+        date = self.request.query_params.get("date")
+
+        queryset = Etc.objects.filter(bank_id=bank_id).order_by("timestamp")
+
+        if date is not None:
+            new_date = datetime.strptime(date, "%Y-%m-%d") + timedelta(days=1)
+            queryset = queryset.filter(bank_id=bank_id, timestamp__gte=date, timestamp__lt=new_date)
+
+        return queryset
+
+
+class PcsListView(ListAPIView):
+    serializer_class = PcsSerializer
+    pagination_class = LargeResultsSetPagination
+
+    def get_queryset(self):
+        operation_num = self.kwargs["operation_num"]
+        bank_id = self.kwargs["bank_id"]
+        date = self.request.query_params.get("date")
+
+        queryset = Pcs.objects.filter(bank_id=bank_id).order_by("timestamp")
+
+        if date is not None:
+            new_date = datetime.strptime(date, "%Y-%m-%d") + timedelta(days=1)
+            queryset = queryset.filter(bank_id=bank_id, timestamp__gte=date, timestamp__lt=new_date)
+
+        return queryset
+
+
+class RackListView(ListAPIView):
+    serializer_class = RackSerializer
+    pagination_class = LargeResultsSetPagination
+
+    def get_queryset(self):
+        operation_num = self.kwargs["operation_num"]
+        bank_id = self.kwargs["bank_id"]
+        date = self.request.query_params.get("date")
+
+        queryset = Rack.objects.filter(bank_id=bank_id).order_by("timestamp")
+
+        if date is not None:
+            new_date = datetime.strptime(date, "%Y-%m-%d") + timedelta(days=1)
+            queryset = queryset.filter(bank_id=bank_id, timestamp__gte=date, timestamp__lt=new_date)
+
+        return queryset
