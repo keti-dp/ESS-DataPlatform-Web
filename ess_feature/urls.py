67d589133fd0cf93ad577d5d338f841cb220c786
임홍휘 <51071806+thinklim@users.noreply.github.com>
2022-02-09 14:23:43 +0900

    Add monitoring log of primary monitoring in data-monitoring (#81)

--- ess_feature/urls.py
+++ ess_feature/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProtectionMapFeatureView
+from .views import ProtectionMapFeatureView, ProtectionMapFeatureAllListView
 
 
 urlpatterns = [
+    path("protectionmap/", ProtectionMapFeatureAllListView.as_view()),
     path("protectionmap/operating-sites/<int:operating_site_id>/", ProtectionMapFeatureView.as_view()),
 ]
--- ess_feature/views.py
+++ ess_feature/views.py
@@ -1,5 +1,6 @@
+from django.core.exceptions import ValidationError
 from rest_framework.generics import ListAPIView
-from rest_framework.views import Response
+from rest_framework.views import Response, status
 from .models import ProtectionMapFeature
 from .serializers import ProtectionMapFeatureSerializer
 
@@ -17,3 +18,44 @@ class ProtectionMapFeatureView(ListAPIView):
         ).order_by("-timestamp")
 
         return queryset
+
+
+class ProtectionMapFeatureAllListView(ListAPIView):
+    serializer_class = ProtectionMapFeatureSerializer
+
+    def get_queryset(self):
+        time = self.request.query_params.get("time")
+        start_time = self.request.query_params.get("start-time")
+        end_time = self.request.query_params.get("end-time")
+
+        # If 'time' parameter is included, other parameters are ignored.
+        if time:
+            queryset = ProtectionMapFeature.objects.filter(timestamp=time).order_by("-timestamp")
+        else:
+            queryset = ProtectionMapFeature.objects.filter(timestamp__gte=start_time, timestamp__lt=end_time).order_by(
+                "-timestamp"
+            )
+
+        return queryset
+
+    def get(self, request, *args, **kwargs):
+        try:
+            return super().get(request, *args, **kwargs)
+        except ValidationError:
+            return Response(
+                {
+                    "code": "400",
+                    "exception": "Validation Error",
+                    "message": "올바른 요청 파라미터를 입력하세요.('time' 형식은 'YYYY-MM-DDThh:mm:ss' 입니다.)",
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        except ValueError:
+            return Response(
+                {
+                    "code": "400",
+                    "exception": "Value Error",
+                    "message": "필수 요청 파라미터를 입력하세요.('time' 또는 'start-time' & 'end-time')",
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
