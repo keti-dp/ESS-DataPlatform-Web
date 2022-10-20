de899ac3a22bda9911823bb0861c8008e7d56746
thinklim <sagalim3983@gmail.com>
2022-10-20 16:18:44 +0900

    Add URL access token in authentication

--- config/authenticate.py
+++ config/authenticate.py
@@ -3,12 +3,18 @@ from rest_framework_simplejwt.authentication import JWTTokenUserAuthentication
 
 class CustomJWTTokenUserAuthentication(JWTTokenUserAuthentication):
     def authenticate(self, request):
-        header = self.get_header(request)
+        access_token_query_param = request.query_params.get("access-token")
 
-        if header is None:
-            raw_token = request.COOKIES.get("access_token") or None
+        # Support to access token in request url or header
+        if access_token_query_param:
+            raw_token = access_token_query_param
         else:
-            raw_token = self.get_raw_token(header)
+            header = self.get_header(request)
+
+            if header is None:
+                raw_token = request.COOKIES.get("access_token") or None
+            else:
+                raw_token = self.get_raw_token(header)
 
         validated_token = self.get_validated_token(raw_token)
 
